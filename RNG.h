40b4c3bb75d0744cb95dda2a999765078a06d4af
jstringer1 <jstringer1@hotmail.com>
2022-05-21 01:17:09 +0100

    Convert outcome to reel positions

--- RNG.h
+++ RNG.h
@@ -3,10 +3,35 @@
 #include <ctime>
 #include <random>
 
+enum OUTCOME {
+	LOSE,
+	TWO_SYMBOLS,
+	THREE_SYMBOLS,
+	THREE_BELLS
+};
+
+enum SYMBOL {
+	CHERRY,
+	BELL,
+	LEMMON,
+	ORANGE,
+	STAR,
+	SEVEN
+};
+
+struct REEL_POSITIONS{
+	SYMBOL reel1;
+	SYMBOL reel2;
+	SYMBOL reel3;
+};
+
 class RNG {
 public:
 	RNG();
+	REEL_POSITIONS generateReelPositions();
+	OUTCOME generateOutcome();
 	unsigned int nextInt(unsigned int exclusiveUpperBound);
 private:
 	std::mt19937 rng;
 };
+
